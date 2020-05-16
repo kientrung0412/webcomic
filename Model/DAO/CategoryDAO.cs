18ebf7a02c01b1c4423072cc42d7b6dedadfb029
hanabi <nguyenkientrung0412@gmail.com>
2020-05-16 12:36:04 +0700

    add CategoryDAO.cs, ComnentDAO.cs, NationDAO.cs

---
+++ Model/DAO/CategoryDAO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using up_down.Models;
+
+namespace Model.DAO
+{
+    public class CategoryDAO
+    {
+        public WCDbContext WcDbContext;
+
+        public CategoryDAO()
+        {
+            WcDbContext = new WCDbContext();
+        }
+
+        public List<category> List()
+        {
+            var list = WcDbContext.category.ToList();
+            return list;
+        }
+
+        public int Add(category category)
+        {
+            var sql = WcDbContext.category.Add(category);
+            var n = WcDbContext.SaveChanges();
+            return n;
+        }
+    }
+}
---
+++ Model/DAO/ComnentDAO.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using up_down.Models;
+
+namespace Model.DAO
+{
+    public class ComnentDAO
+    {
+        public WCDbContext WcDbContext;
+
+        public ComnentDAO()
+        {
+            WcDbContext = new WCDbContext();
+        }
+
+        public List<comment> List(int chapterId)
+        {
+            var list = WcDbContext.comment.Where(comment => comment.StatusCommentId == 1 && comment.ChapterId == chapterId).ToList();
+            return list;
+        }
+    }
+}
