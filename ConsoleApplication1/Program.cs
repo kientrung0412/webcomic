c8dcb0b9f2b4a29c359c43ec2e9c12917a9b6c9c
hanabi <nguyenkientrung0412@gmail.com>
2020-05-17 20:25:51 +0700

    thay đổi database

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Model.DAO;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CategoryDAO categoryDao =new CategoryDAO();
+            categoryDao.List();
+        }
+    }
+}
