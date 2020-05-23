6691c523154661b71549acc87250d60745c2a51b
hanabi <nguyenkientrung0412@gmail.com>
2020-05-24 05:57:49 +0700

    add slide to _Layout.cshtml test

---
+++ WebApplication1/Scripts/slide.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        margin: 10,
+        autoWidth: true,
+        autoHeight: true,
+        responsive: {
+            0: {
+                items: 2
+            },
+            575: {
+                items: 4
+            },
+            991: {
+                items: 5
+            }
+        }
+    });
+});
