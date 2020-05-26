7e46561f527bf7b74072024c4927f9e13392e39f
hanabi <nguyenkientrung0412@gmail.com>
2020-05-27 01:32:28 +0700

    show bg-img

---
+++ WebApplication1/Scripts/load-bg.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    // var urlBg = $('.img-bg').attr("url-bg");
+    // $(this).css(" background-image", "a");
+    // console.log(urlBg);
+
+    $('.img-bg').each(function () {
+        var item =  $(this)
+        var urlBg = item.attr("url-bg");
+        console.log(urlBg);
+        item.css("background-image", urlBg);
+    });
+})
