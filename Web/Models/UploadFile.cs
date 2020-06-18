bd57cf92781a886adae2bec85c415c85d24ec237
hanabi <nguyenkientrung0412@gmail.com>
2020-06-18 20:52:39 +0700

    Check type file

--- Web/Models/UploadFile.cs
+++ Web/Models/UploadFile.cs
@@ -6,49 +6,51 @@ namespace Model.Models
 {
     public class UploadFile
     {
-        public String SetNameFile(HttpPostedFileBase file)
-        {
-            String fileName = String.Format("{0}_{1}", DateTime.Now.ToString("ddMMyy_hhmmss"), file.FileName);
-            return fileName;
-        }
-
         public static Messenger Upload(HttpPostedFileBase file, String filePath, String fileName)
         {
             Messenger messenger = new Messenger();
 
             try
             {
-                if (file.ContentLength > 0)
+                var typeFile = file.ContentType;
+
+                if (typeFile.Contains("image"))
                 {
-
-                    if (!Directory.Exists(filePath))
+                    if (file.ContentLength > 0)
                     {
-                        Directory.CreateDirectory(filePath);
+                        if (!Directory.Exists(filePath))
+                        {
+                            Directory.CreateDirectory(filePath);
+                        }
+
+                        try
+                        {
+                            file.SaveAs(Path.Combine(filePath, fileName));
+                            messenger.Code = 1;
+                            messenger.Mss = fileName;
+
+                            return messenger;
+                        }
+                        catch (Exception e)
+                        {
+                            messenger.Mss = String.Format("Tải lên thất bại!, {0}", e);
+                            messenger.Code = 0;
+
+                            return messenger;
+                        }
                     }
 
-                    try
-                    {
-                        file.SaveAs(Path.Combine(filePath, fileName));
-                        messenger.Code = 1;
-                        messenger.Mss = fileName;
-
-                        return messenger;
-                    }
-                    catch (Exception e)
-                    {
-                        messenger.Mss = String.Format("Tải lên thất bại!, {0}", e);
-                        messenger.Code = 0;
-
-                        return messenger;
-                    }
-                }
-                else
-                {
                     messenger.Mss = "Có lỗi tải lên";
                     messenger.Code = 0;
 
                     return messenger;
                 }
+                
+                messenger.Mss = "Định dạng file không đúng";
+                messenger.Code = 0;
+                
+                return messenger;
+                
             }
             catch (Exception e)
             {
